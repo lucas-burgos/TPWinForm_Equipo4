ceedf0fd9b9ceb2c999dbca3ff4e4ecc0bf11f88
lucacaceres232006-max <lucacaceres232006@gmail.com>
2026-09-09 17:47:35 -0300

    Creación del modelo de clases

---
+++ Modelo/Imagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Imagen
+    {
+        public int Id { get; set; }
+        public int IdArticulo { get; set; }
+        public string ImagenUrl { get; set; }
+    }
+}
